4082a23543d616f2a1c76ee9319e4c328d4f7c18
Syeda Sana Zehra Zaidi <76579833+sanazehra2001@users.noreply.github.com>
2021-11-11 01:04:38 +0500

    Infix to Postfix Conversion and Evaluation

--- StackTemplate.cpp
+++ StackTemplate.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <sstream>	//for converting string to float
+#include <string>
+#include <cmath>	//for using pow method
 using namespace std;
 
 
@@ -13,7 +16,7 @@ class Stack {
 	}
 
 public:
-	Stack(int size) {
+	Stack(int size = 20) {	//creates a stack with size provided or of size 20 otherwise
 		SIZE = size;
 		stack = new T[SIZE];
 	}
@@ -22,15 +25,15 @@ public:
 		return (top == -1);
 	}
 
-    bool peek(T* n) //returns true if there is a value at top of the stack
-    {
-        if (isEmpty()) {
-            return false;
-        }
+	bool peek(T* n) //returns true if there is a value at top of the stack
+	{
+		if (isEmpty()) {
+			return false;
+		}
 
-        *n = stack[top];
-        return true;
-    };
+		*n = stack[top];
+		return true;
+	};
 
 	void push(T value) {      //pushes an element on the top of the stack
 		if (!isFull()) {
@@ -48,7 +51,6 @@ public:
 		}
 		else {
 			T val = stack[top--];
-			cout << val;
 			return val;
 		}
 	}
@@ -60,29 +62,162 @@ public:
 	}
 };
 
-int main() {
-	Stack <int> s1(5);
-	int n = 0;
-	for (int i = 0; i < 5; i++)
-	{
-		cout << i << endl;
-		s1.push(i);
-	}
-	
-	cout << endl;
-	
-	if (s1.peek(&n)) {
-		cout << "peak is " << n << endl;
-	};
-	cout << endl;
+int checkPrecedence(char opr, char top) {		// 0: higher, 1: equal, 2: lower
+	if ((opr == '(') || (opr == '^' && top != '^') || ((opr == '/' || opr == '*') && (top == '+' || top == '-')) || (top == ' ') || (top == '('))
+		return 0;
+	else if (opr == top)
+		return 1;
+	else
+		return 2;
+}
 
-	
-	for (int i = 0; i < 2; i++)
+string removeExtraSpaces(string exp) {	//removes any two consecutive spaces
+	string w_space = "";
+	w_space.push_back(exp[0]);
+
+	for (int i = 1; i < exp.size() + 1; i++) {
+		if (!(w_space[i - 1] == ' ' && exp[i] == ' ')) {
+			w_space.push_back(exp[i]);
+		}
+	}
+	return w_space;
+}
+
+class Postfix {
+	string postfixTerm = "";
+	string postfix = "";
+	Stack <char> operators;
+	Stack <double> evaluation;
+	char popped = ' ';
+	char n = ' ';
+
+public:
+	string convertToPostfix(string infix)		//converts infix to postfix assuming that a valid expression is received
 	{
-		s1.pop();
-		cout << endl;
+		for (int i = 0; i < infix.size() + 1; i++)
+		{
+			if (isdigit(infix[i]))
+				postfixTerm.push_back(infix[i]);	//append each digit to the term
+
+			else if (!isspace(infix[i])) {
+
+				//appends the previous term to the postfix expression
+
+				postfix.append(postfixTerm);
+				if (postfix != "") {
+					postfix.append(" ");
+				}
+				postfixTerm = "";
+
+				// if the stack is empty OR the top of stack is opening bracket OR
+				// incoming operator has higher precedence than the top
+				// then push the incoming operator to the stack
+
+				operators.peek(&n);
+				if (operators.isEmpty() || n == '(' || checkPrecedence(infix[i], n) == 0) {
+					operators.push(infix[i]);
+				}
+
+				// if the incoming symbol is ')', pop the stack and print the operators until the left
+				// parenthesis is found.
+
+				else if (infix[i] == ')') {
+
+					popped = operators.pop();
+
+					while (popped != '(') {
+						postfix.push_back(popped);
+						popped = operators.pop();
+					}
+				}
+
+				else {
+
+					// if the incoming operator has the same precedence with the top of the stack then
+					// pop and print the top of the stack then push the incoming operator
+
+					if (checkPrecedence(infix[i], n) == 1) {
+						postfix.push_back(operators.pop());
+						operators.push(infix[i]);
+					}
+
+					// if the incoming symbol has lower precedence than the top of the stack, pop and
+					// print the top of the stack.Then test the incoming operator against the new top of
+					// the stack
+
+					if (checkPrecedence(infix[i], n) == 2) {
+
+						while (!operators.isEmpty() && checkPrecedence(infix[i], n) == 2) {
+							postfix.push_back(operators.pop());
+							operators.peek(&n);
+						}
+						operators.push(infix[i]);
+					}
+				}
+			}
+		}
+		return removeExtraSpaces(postfix);
 	}
 
-	s1.clear();
-	s1.pop();
+	float evaluateExp(string postfix) {
+		postfixTerm = "";
+		float below = 0;
+		float top = 0;
+		float result;
+		float var = 0;
+
+		for (int i = 0; i < postfix.size() - 1; i++) {
+
+			if (isdigit(postfix[i]))
+				postfixTerm.push_back(postfix[i]);	//append each digit to the term
+
+			else if (isspace(postfix[i])) {
+				if (postfixTerm != "") {	// ignore space after any operator
+					stringstream degree(postfixTerm);	//convert string to double
+					degree >> var;
+					evaluation.push(var);
+					postfixTerm = "";
+				}
+			}
+
+			else {
+				top = evaluation.pop();
+				below = evaluation.pop();
+
+				if (postfix[i] == '+') {
+					result = below + top;
+				}
+				else if (postfix[i] == '-') {
+					result = below - top;
+				}
+				else if (postfix[i] == '*') {
+					result = below * top;
+				}
+				else if (postfix[i] == '/') {
+					result = below / top;
+				}
+				else if (postfix[i] == '^') {
+					result = pow(below, top);
+				}
+				evaluation.push(result);
+			}
+		}
+		return result;
+	}
+};
+
+
+int main()
+{
+	Postfix p;
+	string infix;
+	string postfix;
+	double ans;
+
+	cout << "Enter the infix expression to convert it postfix: ";
+	getline(cin, infix);
+	postfix = p.convertToPostfix(infix);
+	cout << "Postfix Expression: " << postfix << endl;
+	ans = p.evaluateExp(postfix);
+	cout << "Evaluation: " << ans << endl;
 }
